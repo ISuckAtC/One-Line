9e98087c6de9be83bc7c2a1f1031e37bddc4dd92
WizyWoo <kevin.hagfors@gmail.com>
2021-02-02 10:37:01 +0100

    idk

--- Assets/Scripts/MovementScriptVar1.cs
+++ Assets/Scripts/MovementScriptVar1.cs
@@ -37,6 +37,7 @@ public class MovementScriptVar1 : MonoBehaviour
         capsuleCollider = gameObject.GetComponent<CapsuleCollider2D>();
         yGroundCheckOffset = -0.05f;
         groundCheckDist = 0.5f;
+        crouchedMoveDebuf = 1f;
 
     }
 
@@ -67,7 +68,7 @@ public class MovementScriptVar1 : MonoBehaviour
             {
 
                 capsuleCollider.size = new Vector2(1f, 1.98f);
-                spriteRenderer.size = new Vector2(1f, 1.99f);
+                spriteRenderer.size = new Vector2(1f, 1f);
                 yGroundCheckOffset = -0.05f;
                 groundCheckDist = 0.5f;
                 jumpPower = 1f;
