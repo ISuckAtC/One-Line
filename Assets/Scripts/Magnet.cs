79d0ccfd5513471a6afbbe62e2ccfff04fc14093
WizyWoo <kevin.hagfors@gmail.com>
2021-02-08 17:02:46 +0100

    Update Magnet.cs

--- Assets/Scripts/Magnet.cs
+++ Assets/Scripts/Magnet.cs
@@ -13,7 +13,8 @@ public class Magnet : MonoBehaviour
         if (col.transform.parent.GetComponent<Rigidbody2D>() && col.transform.parent.tag == "Line")
         {
 
-            Vector2 posDif = transform.position - col.transform.parent.position; Vector2 posDifNormal = posDif.normalized; col.transform.parent.GetComponent<Rigidbody2D>().velocity = col.transform.parent.GetComponent<Rigidbody2D>().velocity + (posDifNormal * magnetPower);
+            Vector2 posDif = transform.position - col.transform.parent.position; Vector2 posDifNormal = posDif.normalized; 
+            col.transform.parent.GetComponent<Rigidbody2D>().velocity = col.transform.parent.GetComponent<Rigidbody2D>().velocity + (posDifNormal * magnetPower);
 
         }
 
