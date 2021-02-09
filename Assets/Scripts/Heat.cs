f191e0ce805d71e9a180e2e8db9391352244c421
Ross <theporkchopsxd@gmail.com>
2021-02-09 13:33:58 +0100

    Heat waves

---
+++ Assets/Scripts/Heat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heat : MonoBehaviour
+{
+    public float HeatInterval;
+    private BoxCollider2D heatArea;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heatArea = GetComponentInChildren<BoxCollider2D>();
+        InvokeRepeating(nameof(HeatWave), 0, HeatInterval);
+    }
+
+    public void HeatWave()
+    {
+        List<Collider2D> touching = new List<Collider2D>();
+        heatArea.OverlapCollider(new ContactFilter2D(), touching);
+        Line buf;
+        Collider2D[] ice = touching.FindAll(x => x.transform.parent != null && x.transform.parent.TryGetComponent<Line>(out buf) && buf.LineType == LineType.Ice).ToArray();
+        foreach (Collider2D icePiece in ice) Destroy(icePiece.gameObject);
+    }
+}
