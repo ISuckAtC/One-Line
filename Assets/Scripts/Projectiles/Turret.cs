bec4080c1da1d6dbff2f9182df3ac4587585fb09
ENKA TeamLead <345982@student.nord.no>
2021-02-11 10:08:03 +0100

    Improving turrets, adding some folders

---
+++ Assets/Scripts/Projectiles/Turret.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject bullet;
+    public int fireRate = 1;
+
+    void Start()
+    {
+
+        InvokeRepeating(nameof(TurretShoot), 5, fireRate);
+    }
+    private void TurretShoot()
+    {
+        GameObject newBullet = Instantiate(bullet, transform.position + transform.up*1.5f, Quaternion.identity);
+        newBullet.GetComponent<Rigidbody2D>().velocity = transform.up * 3;
+        newBullet.transform.rotation = transform.rotation;
+    }
+}
