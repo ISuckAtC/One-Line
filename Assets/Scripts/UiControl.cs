ca4b087b2dc7ca7b3db12c821c72c75a7adea0db
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2021-02-12 13:15:36 +0100

    main menu

--- Assets/Scripts/UiControl.cs
+++ Assets/Scripts/UiControl.cs
@@ -1,23 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UiControl : MonoBehaviour
 {
 
-    private Canvas canvas;
+    private GameObject inGameUi;
+    private bool inGameUiOnOff;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        canvas = GameObject.FindObjectOfType<Canvas>();
+        inGameUiOnOff = false;
+        inGameUi = GameObject.Find("inGameUi");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            inGameUiOnOff = !inGameUiOnOff;
+
+        inGameUi.SetActive(inGameUiOnOff);
+
     }
+
+    public void MainMenuButton()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void QuitButton()
+    {
+
+        Application.Quit();
+
+    }
+
+    public void LevelSelect(int lvlToLoad)
+    {
+
+        SceneManager.LoadScene(lvlToLoad);
+
+    }
+
 }
