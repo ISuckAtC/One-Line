c6a81f1ae31b461460dbb669a9a85f8508cca0f5
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2021-02-12 15:25:53 +0100

    ui coolsies

--- Assets/Scripts/UiControl.cs
+++ Assets/Scripts/UiControl.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UiControl : MonoBehaviour
 {
 
     private GameObject inGameUi;
     private bool inGameUiOnOff;
+    private GameControl gc;
+    private Text coinsText;
 
     // Start is called before the first frame update
     void Start()
     {
 
+        coinsText = GameObject.Find("CoinsText").GetComponent<Text>();
+        gc = GameObject.Find("GameControl").GetComponent<GameControl>();
         inGameUiOnOff = false;
         inGameUi = GameObject.Find("inGameUi");
 
@@ -26,6 +31,8 @@ public class UiControl : MonoBehaviour
             inGameUiOnOff = !inGameUiOnOff;
 
         inGameUi.SetActive(inGameUiOnOff);
+
+        coinsText.GetComponent<Text>().text = "coins: " + gc.Coins;
 
     }
 
