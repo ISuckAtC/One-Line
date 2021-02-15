cb2be0f3f9d2294f448eb711040ed285237c407e
Ross <theporkchopsxd@gmail.com>
2021-02-15 11:03:04 +0100

    Inkwell tweak

--- Assets/Scripts/Inkwell.cs
+++ Assets/Scripts/Inkwell.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Inkwell : MonoBehaviour
 {
     public LineType lineType;
+    public int Amount;
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            GameControl.main.ModInk(lineType, 1);
+            GameControl.main.ModInk(lineType, Amount);
             Destroy(gameObject);
         }
     }
