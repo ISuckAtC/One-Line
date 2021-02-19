c332e6200a5f33a84227ad26b51ff89db00640fe
CBBA <theporkchopsxd@gmail.com>
2021-02-19 11:57:12 +0100

    Lava death and heat fix

--- Assets/Scripts/Heat.cs
+++ Assets/Scripts/Heat.cs
@@ -9,15 +9,17 @@ public class Heat : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        heatArea = GetComponentInChildren<BoxCollider2D>();
+        heatArea = transform.GetChild(0).GetComponent<BoxCollider2D>();
         InvokeRepeating(nameof(HeatWave), 0, HeatInterval);
     }
 
     public void HeatWave()
     {
         List<Collider2D> touching = new List<Collider2D>();
-        heatArea.OverlapCollider(new ContactFilter2D(), touching);
+        ContactFilter2D a = new ContactFilter2D();
+        heatArea.OverlapCollider((new ContactFilter2D()).NoFilter(), touching);
         Line buf;
+        Debug.Log(touching.Count);
         Collider2D[] ice = touching.FindAll(x => x.transform.parent != null && x.transform.parent.TryGetComponent<Line>(out buf) && buf.LineType == LineType.Ice).ToArray();
         foreach (Collider2D icePiece in ice) Destroy(icePiece.gameObject);
     }
