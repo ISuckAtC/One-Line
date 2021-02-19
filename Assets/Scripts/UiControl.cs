7ce4f734b1d893bc0d0225368378821b7b530e06
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2021-02-19 15:40:14 +0100

    GlowyCoolsies

--- Assets/Scripts/UiControl.cs
+++ Assets/Scripts/UiControl.cs
@@ -13,6 +13,10 @@ public class UiControl : MonoBehaviour
     private GameControl gc;
     private Text coinsText;
     private GameObject FinishUi;
+    private RawImage regGlow, iceGlow, rubGlow, weightGlow;
+    private float fadeValue;
+    private LineType CLT; 
+    private RawImage CGT;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,10 @@ public class UiControl : MonoBehaviour
         PauseGameUi = GameObject.Find("PauseGameUi");
         InGameUi = GameObject.Find("InGameUi");
         FinishUi.SetActive(false);
+        regGlow = GameObject.Find("Regular_Glow").GetComponent<RawImage>();
+        iceGlow = GameObject.Find("Ice_Glow").GetComponent<RawImage>();
+        rubGlow = GameObject.Find("Rubber_Glow").GetComponent<RawImage>();
+        weightGlow = GameObject.Find("Weight_Glow").GetComponent<RawImage>();
 
     }
 
@@ -35,7 +43,7 @@ public class UiControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
             PauseGameUiOnOff = !PauseGameUiOnOff;
 
-        if(InGameUi)
+        if (InGameUi)
         {
 
             PauseGameUi.SetActive(PauseGameUiOnOff);
@@ -43,7 +51,34 @@ public class UiControl : MonoBehaviour
             coinsText.GetComponent<Text>().text = "coins: " + gc.Coins;
 
         }
-        
+
+        fadeValue = Mathf.PingPong(Time.time, 1);
+
+        CLT = LineType.Normal;
+        CGT = regGlow;
+        if (gc.Ink[(int)CLT] <= 0) CGT.enabled = false;
+        else if (gc.Ink[(int)CLT] == 1) { CGT.enabled = true; CGT.CrossFadeAlpha(fadeValue, 1, true); }
+        else CGT.enabled = true;
+
+        CLT = LineType.Ice;
+        CGT = iceGlow;
+        if (gc.Ink[(int)CLT] <= 0) CGT.enabled = false;
+        else if (gc.Ink[(int)CLT] == 1) { CGT.enabled = true; CGT.CrossFadeAlpha(fadeValue, 1, true); }
+        else CGT.enabled = true;
+
+        CLT = LineType.Rubber;
+        CGT = rubGlow;
+        if (gc.Ink[(int)CLT] <= 0) CGT.enabled = false;
+        else if (gc.Ink[(int)CLT] == 1) { CGT.enabled = true; CGT.CrossFadeAlpha(fadeValue, 1, true); }
+        else CGT.enabled = true;
+
+        CLT = LineType.Weight;
+        CGT = weightGlow;
+        if (gc.Ink[(int)CLT] <= 0) CGT.enabled = false;
+        else if (gc.Ink[(int)CLT] == 1) { CGT.enabled = true; CGT.CrossFadeAlpha(fadeValue, 1, true); }
+        else CGT.enabled = true;
+
+
     }
 
     public void MainMenuButton() => SceneManager.LoadScene(0);
