5054b138a39168de03e01d601b6cef6b4aa81fcf
CBBA <theporkchopsxd@gmail.com>
2021-02-26 11:32:49 +0100

    Buttons

---
+++ Assets/Scripts/ActivatorButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatorButton : MonoBehaviour
+{
+    public GameObject[] Activatables;
+
+    public void OnTriggerEnter2D(Collider col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Ground")) return;
+        foreach(GameObject activatable in Activatables)
+        {
+            activatable.GetComponent<IActivatable>().Activate();
+        }
+    }
+}
