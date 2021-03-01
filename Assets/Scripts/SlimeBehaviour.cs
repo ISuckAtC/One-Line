becc50153f0162d696e91f597651e7cea2b398e4
WizyWoo <kevin.hagfors@gmail.com>
2021-03-01 10:01:09 +0100

    SlimeBoio

--- Assets/Scripts/SlimeBehaviour.cs
+++ Assets/Scripts/SlimeBehaviour.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class SlimeBehaviour : MonoBehaviour
 {
-
+    [SerializeField]
     private GameObject Player;
     private LayerMask EnemyMask;
     private Vector2 SlimeMoveDir, TempVector2;
+    [SerializeField]
     private float AggroTimer, JumpTimer, LastAttackTimer;
     public float AggroTime, JumpTime, ForwardJump, Jumpheight, AttackSpeed;
+    [SerializeField]
     private bool HasSeenPlayer;
+    [SerializeField]
     private Rigidbody2D RB2D;
+    [SerializeField]
     private PlayerController PlayerControl;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        Player = GameObject.Find("Player");
+        Player = GameObject.Find("Player 1");
         RB2D = gameObject.GetComponent<Rigidbody2D>();
-        EnemyMask = ~(1 << LayerMask.NameToLayer("Player"));
+        EnemyMask = ~(1 << LayerMask.NameToLayer("Enemy"));
         PlayerControl = Player.GetComponent<PlayerController>();
 
     }
@@ -31,32 +35,36 @@ public class SlimeBehaviour : MonoBehaviour
 
         RaycastHit2D hit2D;
 
-        if (hit2D = Physics2D.Linecast(transform.position, Player.transform.position, EnemyMask))
+        hit2D = Physics2D.Linecast(transform.position, Player.transform.position, EnemyMask);
+
+        Debug.DrawLine(transform.position, Player.transform.position, Color.green, 5f);
+
+        if (hit2D.collider.name == ("Player 1"))
         {
 
-            if (hit2D.collider.name == ("Player"))
+            Debug.Log("Hit");
+
+            HasSeenPlayer = true;
+
+            TempVector2 = Player.transform.position - transform.position;
+
+            if (TempVector2.normalized.x < 0)
+                SlimeMoveDir = -Vector2.right;
+            else if (TempVector2.normalized.x > 0)
+                SlimeMoveDir = Vector2.right;
+            else
+                SlimeMoveDir = Vector2.zero;
+
+            if (JumpTimer < 0)
             {
 
-                TempVector2 = Player.transform.position - transform.position;
-
-                if (TempVector2.normalized.x < 0)
-                    SlimeMoveDir = -Vector2.right;
-                else if (TempVector2.normalized.x > 0)
-                    SlimeMoveDir = Vector2.right;
-                else
-                    SlimeMoveDir = Vector2.zero;
-
-                if(JumpTimer < 0)
-                {
-
-                    RB2D.velocity = new Vector2(SlimeMoveDir.x * ForwardJump, Jumpheight);
-                    JumpTimer = JumpTime;
-
-                }
+                RB2D.velocity = new Vector2(SlimeMoveDir.x * ForwardJump, Jumpheight);
+                JumpTimer = JumpTime;
 
             }
 
-        } else
+        }
+        else
             HasSeenPlayer = false;
 
         if (HasSeenPlayer)
@@ -64,7 +72,7 @@ public class SlimeBehaviour : MonoBehaviour
         else
             AggroTimer -= Time.deltaTime;
 
-        while (AggroTimer > 0)
+        if (AggroTimer > 0)
         {
 
             JumpTimer -= Time.deltaTime;
@@ -81,7 +89,7 @@ public class SlimeBehaviour : MonoBehaviour
         if(LastAttackTimer < 0)
         {
 
-            if(collision.collider.name == "Player")
+            if(collision.collider.name == "Player 1")
             {
 
                 PlayerControl.Kill(true);
