332f5e620bbd58c7319c7743d45685fdd52ebacf
Ross <theporkchopsxd@gmail.com>
2021-03-08 13:56:57 +0100

    more dia

--- Assets/Scripts/CutScene.cs
+++ Assets/Scripts/CutScene.cs
@@ -7,17 +7,21 @@ public class CutScene : MonoBehaviour
 {
     public Dialogue[] Dialogues;
     public float DialogueTranscisionSpeed;
+    public float DialoguePopupSpeed;
     private GameObject DialogueBoxEnemy, DialogueBoxHero;
+    private Animator EnemyDialogueAnim, HeroDialogueAnim;
     private Text DialogueBoxEnemyText, DialogueBoxHeroText;
     private AudioSource audioSource;
     public void Start()
     {
         audioSource = Camera.main.GetComponent<AudioSource>();
-        //DialogueBoxEnemy = UiControl.main.Dialogue
-        //DialogueBoxHero = UiControl.main.Dialogue
+        DialogueBoxEnemy = UiControl.main.DialogueBoxEnemy;
+        DialogueBoxHero = UiControl.main.DialogueBoxHero;
+        EnemyDialogueAnim = DialogueBoxEnemy.GetComponent<Animator>();
+        HeroDialogueAnim = DialogueBoxHero.GetComponent<Animator>();
         DialogueBoxEnemyText = DialogueBoxEnemy.transform.GetChild(0).GetComponent<Text>();
         DialogueBoxHeroText = DialogueBoxHero.transform.GetChild(0).GetComponent<Text>();
-        
+
         foreach(Dialogue d in Dialogues) d.Load();
     }
     public void OnTriggerEnter2D(Collider2D col)
@@ -32,8 +36,25 @@ public class CutScene : MonoBehaviour
     {
         for(int i = 0; i < Dialogues.Length; ++i)
         {
-            Dialogues[i].Speak(Dialogues[i].Enemy ? DialogueBoxEnemyText : DialogueBoxHeroText, audioSource);
-            yield return new WaitForSeconds(DialogueTranscisionSpeed);
+            DialogueBoxEnemyText.text = "";
+            DialogueBoxHeroText.text = "";
+            if (Dialogues[i].Enemy)
+            {
+                EnemyDialogueAnim.SetTrigger(0);
+                yield return new WaitForSeconds(DialoguePopupSpeed);
+                Dialogues[i].Speak(DialogueBoxEnemyText, audioSource);
+                EnemyDialogueAnim.SetTrigger(0);
+                yield return new WaitForSeconds(DialogueTranscisionSpeed);
+            }
+            else
+            {
+                HeroDialogueAnim.SetTrigger(0);
+                yield return new WaitForSeconds(DialoguePopupSpeed);
+                Dialogues[i].Speak(DialogueBoxHeroText, audioSource);
+                HeroDialogueAnim.SetTrigger(0);
+                yield return new WaitForSeconds(DialogueTranscisionSpeed);
+            }
+            
         }
     }
 }
