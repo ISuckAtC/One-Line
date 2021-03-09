11cc8a983861abdfe8a01640351bc0c7cc1bb781
Ross <theporkchopsxd@gmail.com>
2021-03-09 11:29:04 +0100

    enemy damage and base class

--- Assets/Scripts/Projectiles/Turret.cs
+++ Assets/Scripts/Projectiles/Turret.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour
+public class Turret : Enemy
 {
     public GameObject bullet;
     public float fireRate = 1;
     public float waitBeforeStart = 8;
     public float bulletSpeed = 6;
-    void Start()
+    new public void Start()
     {
-
+        base.Start();
         InvokeRepeating(nameof(TurretShoot), waitBeforeStart, fireRate);
     }
     private void TurretShoot()
@@ -18,5 +18,10 @@ public class Turret : MonoBehaviour
         GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
         newBullet.GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed;
         newBullet.transform.rotation = transform.rotation;
+    }
+
+    public override void Death()
+    {
+        Destroy(gameObject);
     }
 }
