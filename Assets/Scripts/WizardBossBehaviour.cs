5a65d7e1a2d4bb52ccfe6c911789fa32da9e5884
WizyWoo <kevin.hagfors@gmail.com>
2021-03-15 11:53:12 +0100

    changes be happening

--- Assets/Scripts/WizardBossBehaviour.cs
+++ Assets/Scripts/WizardBossBehaviour.cs
@@ -11,23 +11,31 @@ public class WizardBossBehaviour : MonoBehaviour
 
     "Rythm": Fireballs - Dash - SlimeStorm while laughing Manically - Use cannon during SlimeStorm.*/
 
-    public float maxMove;
+    public float maxMoveDist, moveSpeed;
     LayerMask pathBlockingElements;
-    private bool pathBlocked;
+    private bool pathBlocked, DestinationChange;
     private int executions;
+    private Vector2 Destination, FireballAttackPos;
 
 
     void Start()
     {
         
-
+        pathBlockingElements = 1 << LayerMask.NameToLayer("Line");
 
     }
 
     void Update()
     {
-        
 
+        if(Input.GetKey(KeyCode.L)) WizardMove();
+
+    }
+
+    void FixedUpdate()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, Destination, moveSpeed);
 
     }
 
@@ -37,17 +45,33 @@ public class WizardBossBehaviour : MonoBehaviour
         Vector2 moveToNewPos;
         RaycastHit2D hit2D;
 
-        moveToNewPos = new Vector2(transform.position.x + Random.Range(-maxMove, maxMove), transform.position.y + Random.Range(-maxMove, maxMove));
+        moveToNewPos = new Vector2(transform.position.x + Random.Range(-maxMoveDist, maxMoveDist), transform.position.y + Random.Range(-maxMoveDist, maxMoveDist));
 
         if(hit2D = Physics2D.Linecast(transform.position, moveToNewPos, pathBlockingElements)) pathBlocked = true;
+        else pathBlocked = false;
+
+        Debug.DrawRay(transform.position, moveToNewPos, Color.green, 5);
 
         if(!pathBlocked)
         {
 
-            
+            executions = 0;
+            Destination = moveToNewPos;
 
         }
-        else if(pathBlocked && executions < 50) WizardMove();
+        else if(pathBlocked && executions < 50)
+        {
+
+            executions++;
+            WizardMove();
+
+        }
+        else
+        {
+
+            executions = 0;
+
+        }
 
     }
 
