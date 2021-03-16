a498f3393d134ab2303da334ba7a5c6b454677a6
Chandler <345982@student.nord.no>
2021-03-16 14:24:10 +0100

    Rubber 5 and AEenemy

---
+++ Assets/Scripts/ActivatorAreaEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ActivatorAreaEnemy : MonoBehaviour
+{
+    public GameObject CheckMark;
+    public GameObject[] Enemies;
+    public GameObject[] Activatables;
+    bool once;
+    private void FixedUpdate()
+    {
+        if (!Enemies.Any(x => x != null) && once == false) foreach (GameObject activatable in Activatables)
+        {
+            activatable.GetComponent<IActivatable>().Activate();
+            once = true;
+                CheckMark.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+}
