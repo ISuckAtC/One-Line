348cf9bf13ae3657e10dbf2d1ce9c1d36ad5abe1
WizyWoo <kevin.hagfors@gmail.com>
2021-03-29 14:40:03 +0200

    begun work on new MainMenu
    it's ass at the moment :3

---
+++ Assets/Scripts/LevelPreviewSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPreviewSelector : MonoBehaviour
+{
+
+    public int LevelPreviewNumber;
+    private MainMenuController mmc;
+
+    void Start()
+    {
+
+        mmc = GameObject.Find("MainMenuCanvas").GetComponent<MainMenuController>();
+
+    }
+
+    public void OpenPreview()
+    {
+
+        mmc.LevelPreview(LevelPreviewNumber);
+
+    }
+
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public string[] LevelNames;
+    public Sprite[] LevelPreviewImage;
+    public string[] LevelPreviewText;
+    public GameObject MainMenuCursor;
+    public Text PreviewName, PreviewText, GoToLevelButtonText;
+    public Image PreviewImage;
+    private Vector3 mousePosition;
+    private int levelNumSelected;
+
+    void Update()
+    {
+
+        mousePosition = Input.mousePosition;
+        MainMenuCursor.transform.position = mousePosition;
+
+    }
+
+    public void LevelPreview(int lvlNum)
+    {
+
+        levelNumSelected = lvlNum;
+
+        PreviewName.text = LevelNames[lvlNum-1];
+        PreviewImage.sprite = LevelPreviewImage[lvlNum-1];
+        PreviewText.text = LevelPreviewText[lvlNum-1];
+        GoToLevelButtonText.text = "Play Level: " + lvlNum;
+
+    }
+
+    public void GoToLevel()
+    {
+
+        SceneManager.LoadScene(levelNumSelected);
+
+    }
+
+}
