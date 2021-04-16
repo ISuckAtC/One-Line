ad49f38ddefe834f0fd83941fb373761da827970
WizyWoo <kevin.hagfors@gmail.com>
2021-04-16 12:21:03 +0200

    hm

--- Assets/Scripts/LevelBGLoader.cs
+++ Assets/Scripts/LevelBGLoader.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class LevelBGLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Sprite[] sprites;
+    public bool BossLevel;
+
     void Start()
     {
-        
+        if(BossLevel)
+            sprites = Resources.LoadAll("BossBackGrounds", typeof (Sprite)).Cast<Sprite>().ToArray();
+        else
+            sprites = Resources.LoadAll("BackGrounds", typeof (Sprite)).Cast<Sprite>().ToArray();
+        Debug.Log(sprites.Length);
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
