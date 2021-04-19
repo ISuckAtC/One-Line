fe8dd5fce5f1726b84e1d6a79d4737b5b049175b
Chandler <345982@student.nord.no>
2021-04-19 13:32:52 +0200

    New level ItsAlive

---
+++ Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    public Transform topPoint;
+    public Transform bottomPoint;
+    public float speed;
+
+    float timePassed;
+    private Vector2 top;
+    private Vector2 bot;
+    // Update is called once per frame
+    private void Start()
+    {
+        top = topPoint.position;
+        bot = bottomPoint.position;
+    }
+    void Update()
+    {
+        timePassed += Time.deltaTime/speed;
+        gameObject.transform.position = Vector2.Lerp(top, bot, Mathf.PingPong(timePassed, 1f));
+    }
+}
