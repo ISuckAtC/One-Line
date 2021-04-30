c6892f200046f2359f390346a417a228e77c9af1
Mathea <77966258+trampo3@users.noreply.github.com>
2021-04-30 10:46:09 +0200

    oo

---
+++ Assets/Scripts/SpikeWall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeWall : MonoBehaviour, IActivatable
+{
+    bool go;
+
+    void FixedUpdate()
+    {
+        if (go == true)
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * 3);
+        }
+    }
+
+    public void Activate()
+    {
+        Debug.Log(gameObject.name + " activated");
+        go = !go;
+    }
+}
