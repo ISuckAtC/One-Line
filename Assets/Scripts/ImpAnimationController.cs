19ac1d432f4eb052b0ad10eb7a702beef0dcad92
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2021-05-21 11:25:34 +0200

    shaiz

--- Assets/Scripts/ImpAnimationController.cs
+++ Assets/Scripts/ImpAnimationController.cs
@@ -5,42 +5,44 @@ using UnityEngine;
 public class ImpAnimationController : MonoBehaviour
 {
     public Animator impAnim;
-    // Start is called before the first frame update
-//    void Start()
-//    {
-//
-//    }
-//
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if()
-//        {
-//            impAnim.SetBool("IsAttacking", true); //for fire ball
-//            impAnim.SetBool("IsDashing", false);
-//            impAnim.SetBool("IsDying", false);
-//            impAnim.SetBool("IsIdle", false);
-//        }
-//        if()
-//        {
-//            impAnim.SetBool("IsAttacking", false);
-//            impAnim.SetBool("IsDashing", true); //For dash attack
-//            impAnim.SetBool("IsDying", false);
-//            impAnim.SetBool("IsIdle", false);
-//        }
-//        if()
-//        {
-//            impAnim.SetBool("IsAttacking", false);
-//            impAnim.SetBool("IsDashing", false);
-//            impAnim.SetBool("IsDying", true);
-//            impAnim.SetBool("IsIdle", false);
-//        }
-//        else
-//        {
-//            impAnim.SetBool("IsAttacking", false);
-//            impAnim.SetBool("IsDashing", false);
-//            impAnim.SetBool("IsDying", false);
-//            impAnim.SetBool("IsIdle", true);
-//        }
-//    }
+    
+    public void Idle()
+    {
+        Debug.Log("Imp Idling");
+        impAnim.SetBool("IsAttacking", false);
+        impAnim.SetBool("IsDashing", false);
+        impAnim.SetBool("IsDying", false);
+        impAnim.SetBool("IsIdle", true);
+
+    }
+
+    public void Attacking()
+    {
+        Debug.Log("Imp Attacking");
+        impAnim.SetBool("IsAttacking", true);
+        impAnim.SetBool("IsDashing", false);
+        impAnim.SetBool("IsDying", false);
+        impAnim.SetBool("IsIdle", false);
+
+    }
+
+    public void Dashing()
+    {
+        Debug.Log("Imp Dashing");
+        impAnim.SetBool("IsAttacking", false);
+        impAnim.SetBool("IsDashing", true);
+        impAnim.SetBool("IsDying", false);
+        impAnim.SetBool("IsIdle", false);
+
+    }
+
+    public void Dying()
+    {
+
+        impAnim.SetBool("IsAttacking", false);
+        impAnim.SetBool("IsDashing", false);
+        impAnim.SetBool("IsDying", true);
+        impAnim.SetBool("IsIdle", false);
+
+    }
 }
