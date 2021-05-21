719cab932b75cede422169290f24ad86116fc399
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2021-05-21 12:01:09 +0200

    ass

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject ResolutionButton;
+    public Resolution[] resolutions;
+    private List<GameObject> buttons;
+
     void Start()
     {
-        
+
+        resolutions = Screen.resolutions;
+
+        buttons = new List<GameObject>();
+
+        buttons.Add(ResolutionButton);
+
+        for(int i = 0; i < resolutions.Length - 2; i++)
+        {
+
+            GameObject resButton = Instantiate(ResolutionButton, transform);
+            buttons.Add(resButton);
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
