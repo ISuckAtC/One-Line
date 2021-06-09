1c48f8242b61662b80894d4a490588c16ef1674e
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2021-06-09 23:19:50 +0200

    should be done now

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -8,7 +8,6 @@ using System.Linq;
 public class MainMenuController : MonoBehaviour
 {
 
-    public InputField tempNameField;
     public GameObject[] LevelButtons;
     public bool ExcludeLevelNames;
     public string[] LevelNames;
@@ -145,13 +144,6 @@ public class MainMenuController : MonoBehaviour
 
         mousePosition = Input.mousePosition;
         MainMenuCursor.transform.position = mousePosition;
-
-    }
-
-    public void PublishTime()
-    {
-
-        GameControl.PostTime(tempNameField.text, gameData.TotalRunTime, false);
 
     }
 
